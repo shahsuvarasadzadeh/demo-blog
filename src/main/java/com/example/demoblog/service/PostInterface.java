6313c7820f88320ff89dcef352a891f237e521a8
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-27 00:23:01 +0400

    creating Update interface

--- src/main/java/com/example/demoblog/service/PostInterface.java
+++ src/main/java/com/example/demoblog/service/PostInterface.java
@@ -2,11 +2,13 @@ package com.example.demoblog.service;
 
 import com.example.demoblog.dto.PostCreateDTO;
 import com.example.demoblog.dto.PostDTO;
+import com.example.demoblog.dto.PostUpdateDTO;
 
 import java.util.List;
 
 public interface PostInterface {
     PostDTO createPost(PostCreateDTO postCreateDTO);
     List<PostDTO> getPost();
-    void deleteById(Long id);
+    void deletePostById(Long id);
+    PostDTO updatePost(Long id, PostUpdateDTO postUpdateDTO);
 }
