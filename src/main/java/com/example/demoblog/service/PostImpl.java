a2523fe4176b5de56505137698356ab3a1451463
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-27 00:23:23 +0400

    implementing Update interface

--- src/main/java/com/example/demoblog/service/PostImpl.java
+++ src/main/java/com/example/demoblog/service/PostImpl.java
@@ -1,15 +1,15 @@
 package com.example.demoblog.service;
 
-import com.example.demoblog.GenericResponse;
 import com.example.demoblog.dto.PostCreateDTO;
 import com.example.demoblog.dto.PostDTO;
+import com.example.demoblog.dto.PostUpdateDTO;
+import com.example.demoblog.exception.NotFoundException;
 import com.example.demoblog.model.PostEntity;
 import com.example.demoblog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,8 +33,21 @@ public class PostImpl implements PostInterface{
     }
 
     @Override
-    public void deleteById(Long id) {
-        final PostEntity postEntity=postRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException())
+    public void deletePostById(Long id) {
+        final PostEntity postEntity = postRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("user not found"));
+        postRepository.deleteById(postEntity.getId());
     }
+
+    @Override
+    public PostDTO updatePost(Long id, PostUpdateDTO postUpdateDTO) {
+        final PostEntity postEntity=postRepository.
+                findById(id).orElseThrow(() -> new NotFoundException("user not found"));
+        postEntity.setTitle(postUpdateDTO.getTitle());
+        postEntity.setBody(postUpdateDTO.getBody());
+
+        PostEntity updatePost=postRepository.save(postEntity);
+        return PostDTO.of(updatePost);
+    }
+
 }
