51c1ae49ec7d6fd7cedb025541a5103c09ac5640
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-02-27 00:21:18 +0400

    NotFoundException exception

--- src/main/java/com/example/demoblog/exception/NotFoundException.java
+++ src/main/java/com/example/demoblog/exception/NotFoundException.java
@@ -1,4 +1,13 @@
 package com.example.demoblog.exception;
 
-public class NotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
